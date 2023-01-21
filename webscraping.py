7fc6bb99519a6debedebfffd51d3c32b70edfee7
George <george.j.lindley@gmail.com>
2023-01-21 12:56:54 +0300

    webscraping now working

--- webscraping.py
+++ webscraping.py
@@ -1,6 +1,11 @@
-from webscraping import webdriver
+from selenium import webdriver
+from time import sleep 
+from selenium.webdriver.chrome.service import Service
 
-Path = "/Applications/chromedriver.exe"
-driver = webdriver.Chrome(Path)
+#Path = "/Applications/chromedriver.exe"
+
+driver_service = Service(executable_path="/Applications/chromedriver.exe" )
+driver = webdriver.Chrome(service=driver_service)
 
 driver.get("https://www.english.com/gse/teacher-toolkit/user/lo")
+sleep(10)
